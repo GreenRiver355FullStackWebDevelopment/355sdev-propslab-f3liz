860b421839a3b4608ee5606678f5dd11f3c6d7dd
f3liz <felixchen14@gmail.com>
2025-04-17 11:23:58 -0700

    comments

--- my-react-app/src/components/Recipe.jsx
+++ my-react-app/src/components/Recipe.jsx
@@ -1,4 +1,4 @@
-import {Typography} from '@mui/material'; // import to use Typography
+import {Typography} from '@mui/material'; // import to use Typography for text styles
 import DinnerDiningIcon from '@mui/icons-material/DinnerDining'; // icon
 import EggIcon from '@mui/icons-material/Egg'; // icon
 
@@ -8,7 +8,7 @@ const Recipe = ({ recipe:{name, ingredients, instructions, optionalGlaze}}) => {
             <Typography variant='h2' sx={{ fontWeight: 'bold', color: 'orange'}}>{name} <DinnerDiningIcon fontSize='large'/></Typography>
             <Typography variant='body1' sx={{ fontWeight: 'bold', color: 'yellow'}}><EggIcon/> Ingredients: {ingredients}</Typography>
             <Typography variant='body1' sx={{ fontWeight: 'bold'}}>Instructions: {instructions}</Typography>
-            {optionalGlaze && ( // to include glaze, had to look this up
+            {optionalGlaze && ( // to include glaze, had to look this up, && checks to see if optionalGlaze exists then will render everything after && . Similar to if statement
                 <div>
                     <Typography variant='h5' sx={{ fontWeight:'bold', color: 'orange'}}>Optional Glaze: {optionalGlaze.name}</Typography>
 
